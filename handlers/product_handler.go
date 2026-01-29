7a0a0bfdb8782f0290bfd115c46a64559033758f
Erik Sanjaya <eriiksanjaya@gmail.com>
2026-01-29 11:03:48 +0700

    feat: add product service GetAll and Create

--- handlers/product_handler.go
+++ handlers/product_handler.go
@@ -1,1 +1,58 @@
 package handlers
+
+import (
+	"encoding/json"
+	"kasir-api/models"
+	"kasir-api/services"
+	"net/http"
+)
+
+type ProductHandler struct {
+	service *services.ProductService
+}
+
+func NewProductHandler(service *services.ProductService) *ProductHandler {
+	return &ProductHandler{service: service}
+}
+
+func (h *ProductHandler) HandleProducts(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		h.GetAll(w, r)
+	case http.MethodPost:
+		h.Create(w, r)
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+}
+
+func (h *ProductHandler) GetAll(w http.ResponseWriter, r *http.Request) {
+	products, err := h.service.GetAll()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(products)
+}
+
+func (h *ProductHandler) Create(w http.ResponseWriter, r *http.Request) {
+	var product models.Product
+	err := json.NewDecoder(r.Body).Decode(&product)
+	if err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	err = h.service.Create(&product)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(product)
+
+}
