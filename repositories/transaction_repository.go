bcf3c2c8f83170355d00a96c1c6f9a352753e9eb
Erik Sanjaya <eriiksanjaya@gmail.com>
2026-02-09 10:46:49 +0700

    feat add: transaction

---
+++ repositories/transaction_repository.go
@@ -0,0 +1,109 @@
+package repositories
+
+import (
+	"database/sql"
+	"fmt"
+	"kasir-api/models"
+)
+
+type TransactionRepository struct {
+	db *sql.DB
+}
+
+func NewTransactionRepository(db *sql.DB) *TransactionRepository {
+	return &TransactionRepository{db: db}
+}
+
+func (repo *TransactionRepository) CreateTransaction(items []models.CheckoutItem) (*models.Transaction, error) {
+
+	var (
+		res *models.Transaction
+	)
+
+	tx, err := repo.db.Begin()
+	if err != nil {
+		return nil, err
+	}
+	defer tx.Rollback()
+
+	totalAmount := 0
+	details := make([]models.TransactionDetail, 0)
+
+	for _, item := range items {
+		var price, stock int
+		var name string
+
+		err := tx.QueryRow(`
+			SELECT name, price, stock
+			FROM products
+			WHERE id = $1
+			FOR UPDATE
+		`, item.ProductID).Scan(&name, &price, &stock)
+
+		if err == sql.ErrNoRows {
+			return nil, fmt.Errorf("product id %d not found", item.ProductID)
+		}
+		if err != nil {
+			return nil, err
+		}
+
+		if stock < item.Quantity {
+			return nil, fmt.Errorf("stock not enough for product %s", name)
+		}
+
+		subtotal := price * item.Quantity
+		totalAmount += subtotal
+
+		_, err = tx.Exec(
+			"UPDATE products SET stock = stock - $1 WHERE id = $2",
+			item.Quantity, item.ProductID,
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		details = append(details, models.TransactionDetail{
+			ProductID:   item.ProductID,
+			ProductName: name,
+			Quantity:    item.Quantity,
+			Subtotal:    subtotal,
+		})
+	}
+
+	var transactionID int
+	err = tx.QueryRow(
+		"INSERT INTO transactions (total_amount) VALUES ($1) RETURNING id",
+		totalAmount,
+	).Scan(&transactionID)
+	if err != nil {
+		return nil, err
+	}
+
+	for i := range details {
+		details[i].TransactionID = transactionID
+
+		_, err = tx.Exec(
+			"INSERT INTO transaction_details (transaction_id, product_id, quantity, subtotal) VALUES ($1, $2, $3, $4)",
+			transactionID,
+			details[i].ProductID,
+			details[i].Quantity,
+			details[i].Subtotal,
+		)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if err := tx.Commit(); err != nil {
+		return nil, err
+	}
+
+	res = &models.Transaction{
+		ID:          transactionID,
+		TotalAmount: totalAmount,
+		Details:     details,
+	}
+
+	return res, nil
+
+}
